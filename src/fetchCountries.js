335155f53574ee961492db2059f56b3affd3c000
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-09-18 23:38:30 +0300

    add logic of checks input

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,9 @@
+import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+defaultModules.set(PNotifyMobile, {});
+
 import refs from './refs.js'
 import template from "./template.hbs"
 
@@ -10,18 +16,24 @@ export default
         return data;
     }).then(array => {
         let result = array.map(elem => {
-            if (array.length <= 10 & array.length >=2) {
-                return `<li>${elem.name}</li>`
+            if (array.length <= 10 & array.length >= 2) {
+                return `<li class="list-item">${elem.name}</li>`
             }
             if (array.length === 1) {
-                console.log(array)
                 let markup = template(array);
                 return markup;
             }
-            
+           
         }).join('')
-        console.log(array)
-         refs.countriesList.insertAdjacentHTML('beforeend', result);
+        // console.log(array)
+        refs.countriesList.insertAdjacentHTML('beforeend', result);
+    }).catch(err => {
+        // alert ("nono")
+        if (err.message === "404") {
+            error({
+                text: 'Too many matches found. Please enter a more specific query!',
+            })
+        }
     });
 
 }
