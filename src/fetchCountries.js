bcc3b56dacf49b2bcff0d724f1ea25bd5b55eb7f
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-09-19 18:53:01 +0300

    function "fetchCountries" in a separate file

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,60 +1,9 @@
-import { info, error, success, defaultModules, Stack } from '@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
-defaultModules.set(PNotifyMobile, {});
-
-import refs from './refs.js'
-import template from "./template.hbs"
-
-const myStack = new Stack({
-    dir1: 'up',
-    maxOpen: 1,
-    maxStrategy: 'close',
-    modal: true,
-    overlayClose: true
-});
-
-export default
-    function fetchCountries(searchQuery) {
-    fetch(searchQuery).then(response => {
-        return response.json()
-    }).then(data => {
-        return data;
-    }).then(array => {
-        let result = array.map(elem => {
-            if (array.length <= 10 & array.length >= 2) {
-                     info({
-                    text: "Please, specify search options!",
-                    stack: myStack
-                });           
-                
-                return `<li class="list-item">${elem.name}</li>`
-            }
-            
-            if (array.length === 1) {
-                let markup = template(array);
-                success({
-                    text: "You found the country!",
-                    stack: myStack
-                });
-                return markup;
-            }
-           
-        }).join('')
-        if (array.length > 10) {
-            // console.log(array)
-            throw error
-        }
-        refs.countriesList.insertAdjacentHTML('beforeend', result);
-    }).catch(error => {
-        
-            error({
-            text: "Too many matches found. Please enter a more specific query!",
-            stack: myStack
-        });
-        
-        
-    });
-
+export default function fetchCountries(searchQuery) {
+    if (searchQuery) {
+        return fetch(searchQuery)
+            .then(response => {
+            if (response.ok) return response.json();
+            throw new Error('ERROR fetching data');
+            })
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { alert, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import { info, error, success, defaultModules, Stack } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
@@ -7,16 +7,57 @@ defaultModules.set(PNotifyMobile, {});
 import debounce from 'lodash/debounce';
 import fetchCountries from './fetchCountries.js'
 import refs from './refs.js'
+import template from "./template.hbs"
 
+const myStack = new Stack({
+    dir1: 'up',
+    maxOpen: 1,
+    maxStrategy: 'close',
+    modal: true,
+    overlayClose: true
+});
 
-let searchQuery = 'Uk';
 const endPoint = '/name';
 
 refs.mainInput.addEventListener('input', debounce(onEnterInput, 1000));
 
 function onEnterInput(e) {
-    searchQuery = e.target.value;
+    const searchQuery = e.target.value.trim();
     let URL = `https://restcountries.eu/rest/v2${endPoint}/${searchQuery}`;
     refs.countriesList.innerHTML = ('');
-    fetchCountries(URL);
+    //function to fetch countries
+    fetchCountries(URL).then(data => {
+        return data;
+    }).then(array => {
+        let result = array.map(elem => {
+            if (array.length <= 10 & array.length >= 2) {
+                     info({
+                    text: "Please, specify search options!",
+                    stack: myStack
+                });           
+                
+                return `<li class="list-item">${elem.name}</li>`
+            }
+            
+            if (array.length === 1) {
+                let markup = template(array);
+                success({
+                    text: "You found the country!",
+                    stack: myStack
+                });
+                return markup;
+            }
+           
+        }).join('')
+        if (array.length > 10) {
+            // console.log(array)
+            throw error
+        }
+        refs.countriesList.insertAdjacentHTML('beforeend', result);
+    }).catch(error => {   
+            error({
+            text: "Too many matches found. Please enter a more specific query!",
+            stack: myStack
+        });
+    });
 }
